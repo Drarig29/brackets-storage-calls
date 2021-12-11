c55ccb10de8d60e5480d1c5566642eb1e5f73172
Corentin Girard <corentingirard.dev@gmail.com>
2021-12-11 19:33:31 +0100

    List arguments

--- main.ts
+++ main.ts
@@ -1,132 +1,38 @@
-import * as ts from "typescript";
-import * as fs from "fs";
+import { Project, SyntaxKind } from 'ts-morph';
 
-interface DocEntry {
-  name?: string;
-  fileName?: string;
-  documentation?: string;
-  type?: string;
-  constructors?: DocEntry[];
-  parameters?: DocEntry[];
-  returnType?: string;
-}
+function getStorageCalls(tsConfigFilePath: string): void {
+  const project = new Project({
+    tsConfigFilePath,
+  });
 
-/** Generate documentation for all classes in a set of .ts files */
-function generateDocumentation(
-  fileNames: string[],
-  options: ts.CompilerOptions
-): void {
-  // Build a program using the set of root file names in fileNames
-  let program = ts.createProgram(fileNames, options);
+  const typeDefinitions = project.getSourceFile('types.ts');
+  if (!typeDefinitions) {
+    throw Error('Type definition file not found.');
+  }
 
-  // Get the checker, we will use it to find more about classes
-  let checker = program.getTypeChecker();
-  let output: DocEntry[] = [];
+  const crudInterface = typeDefinitions
+    .getInterfaces()
+    .find((iface) => iface.getName() === 'CrudInterface');
 
-  // Visit every sourceFile in the program
-  for (const sourceFile of program.getSourceFiles()) {
-    if (!sourceFile.isDeclarationFile) {
-      // Walk the tree to search for classes
-      ts.forEachChild(sourceFile, visit);
+  if (!crudInterface) {
+    throw Error('CrudInterface not found.');
+  }
+
+  const crudMethods = crudInterface.getMethods();
+
+  for (const method of crudMethods) {
+    const references = method.findReferencesAsNodes();
+
+    for (const reference of references) {
+      const callExpression = reference.getFirstAncestorByKind(
+        SyntaxKind.CallExpression
+      );
+
+      if (!callExpression) continue;
+
+      console.log(callExpression.getArguments());
     }
-  }
-
-  // print out the doc
-  fs.writeFileSync("classes.json", JSON.stringify(output, undefined, 4));
-
-  return;
-
-  /** visit nodes finding exported classes */
-  function visit(node: ts.Node) {
-    // Only consider exported nodes
-    // if (!isNodeExported(node)) {
-    //   return;
-    // }
-
-
-    if (ts.isPropertyAccessExpression(node)) {
-      if (node.getText().match(/this\.storage\./)) {
-        const storageMethod = node.getChildAt(2);
-        console.log(storageMethod.getText());
-
-        const symbol = checker.getSymbolAtLocation(storageMethod);
-        
-
-        if (symbol) {
-          console.log(checker.typeToString(
-            checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration!)
-          ));
-        }
-      }
-    } else {
-      ts.forEachChild(node, visit);  
-    }
-
-    // if (ts.isClassDeclaration(node) && node.name && node.name?.escapedText === 'Reset') {
-    
-    // return;
-    // }
-
-
-    // if (ts.isClassDeclaration(node) && node.name) {
-    //   // This is a top level class, get its symbol
-    //   let symbol = checker.getSymbolAtLocation(node.name);
-    //   if (symbol) {
-    //     output.push(serializeClass(symbol));
-    //   }
-    //   // No need to walk any further, class expressions/inner declarations
-    //   // cannot be exported
-    // } else if (ts.isModuleDeclaration(node)) {
-    //   // This is a namespace, visit its children
-    //   ts.forEachChild(node, visit);
-    // }
-  }
-
-  /** Serialize a symbol into a json object */
-  function serializeSymbol(symbol: ts.Symbol): DocEntry {
-    return {
-      name: symbol.getName(),
-      documentation: ts.displayPartsToString(symbol.getDocumentationComment(checker)),
-      type: checker.typeToString(
-        checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration!)
-      )
-    };
-  }
-
-  /** Serialize a class symbol information */
-  function serializeClass(symbol: ts.Symbol) {
-    let details = serializeSymbol(symbol);
-
-    // Get the construct signatures
-    let constructorType = checker.getTypeOfSymbolAtLocation(
-      symbol,
-      symbol.valueDeclaration!
-    );
-    details.constructors = constructorType
-      .getConstructSignatures()
-      .map(serializeSignature);
-    return details;
-  }
-
-  /** Serialize a signature (call or construct) */
-  function serializeSignature(signature: ts.Signature) {
-    return {
-      parameters: signature.parameters.map(serializeSymbol),
-      returnType: checker.typeToString(signature.getReturnType()),
-      documentation: ts.displayPartsToString(signature.getDocumentationComment(checker))
-    };
-  }
-
-  /** True if this is visible outside this file, false otherwise */
-  function isNodeExported(node: ts.Node): boolean {
-    return (
-      (ts.getCombinedModifierFlags(node as ts.Declaration) & ts.ModifierFlags.Export) !== 0 ||
-      (!!node.parent && node.parent.kind === ts.SyntaxKind.SourceFile)
-    );
   }
 }
 
-generateDocumentation(process.argv.slice(2), {
-  target: ts.ScriptTarget.ES5,
-  module: ts.ModuleKind.CommonJS
-});
+getStorageCalls(process.argv[2]);
